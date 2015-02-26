cb4a5d184da6bf177847d0e215af3c78d065c004
om.bhallamudi <om.bhallamudi@gmail.com>
2015-02-26 14:39:16 +0530

    refractor code

--- firefox_ui_tests/remote/security/test_mixed_content.py
+++ firefox_ui_tests/remote/security/test_mixed_content.py
@@ -14,27 +14,22 @@ class TestMixedContent(FirefoxTestCase):
     def test_mixed_content(self):
         with self.marionette.using_context('content'):
             self.marionette.navigate(self.test_url)
+
         favicon = self.browser.navbar.locationbar.favicon
 
         def check_favicon_image(self):
-            favicon_image = self.execute_script("""
-                return arguments[0].ownerDocument.defaultView
-                                   .getComputedStyle(arguments[0])
-                                   .getPropertyValue('list-style-image');
-            """, script_args=[favicon])
-            return "identity-icons-https-mixed-display" in favicon_image
+            favicon_image = self.execute_script("\
+                return arguments[0].ownerDocument.defaultView\
+                                   .getComputedStyle(arguments[0])\
+                                   .getPropertyValue('list-style-image');\
+            ", script_args=[favicon])
+            return 'identity-icons-https-mixed-display' in favicon_image
 
         self.wait_for_condition(check_favicon_image)
 
         identity_popup = self.browser.navbar.locationbar.identity_popup
-        identity_box = identity_popup.box
-        identity_box.click()
+        identity_popup.box.click()
 
-        def identity_popup_displayed(self):
-            return identity_popup.popup.is_displayed()
-
-        self.wait_for_condition(identity_popup_displayed)
-        encryption_label = identity_popup.encryption_label
-        label_text = encryption_label.text
-        prop = self.browser.get_property('identity.broken_loaded')
-        self.assertEqual(label_text, prop)
+        self.wait_for_condition(lambda _: identity_popup.popup.is_displayed())
+        self.assertEqual(identity_popup.encryption_label.text,
+                         self.browser.get_property('identity.broken_loaded'))

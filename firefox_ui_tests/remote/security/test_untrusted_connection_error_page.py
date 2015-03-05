f04764df54afbf908c06c1c17da19c54b459753a
om.bhallamudi <om.bhallamudi@gmail.com>
2015-03-05 22:33:19 +0530

    refractor

--- firefox_ui_tests/remote/security/test_untrusted_connection_error_page.py
+++ firefox_ui_tests/remote/security/test_untrusted_connection_error_page.py
@@ -21,15 +21,13 @@ class TestUntrustedConnectionErrorPage(FirefoxTestCase):
     def test_untrusted_connection_error_page(self):
         # Test the GetMeOutOfHereButton from an Untrusted Error page
         with self.marionette.using_context('content'):
-            self.assertRaises(
-                MarionetteException,
-                self.marionette.navigate,
-                self.test_url
-                )
-            # Wait about 1 second for about:error page to work
+            self.assertRaises(MarionetteException,
+                              self.marionette.navigate, self.test_url)
+            # Wait for the DOM to receive events
             time.sleep(1)
-            button = self.marionette.find_element(By.ID,
-                                                  "getMeOutOfHereButton")
+
+            button = self.marionette.find_element(
+                By.ID, "getMeOutOfHereButton")
             self.assertTrue(button.is_displayed())
             button.click()
             self.assertEqual(self.marionette.get_url(), 'about:home')

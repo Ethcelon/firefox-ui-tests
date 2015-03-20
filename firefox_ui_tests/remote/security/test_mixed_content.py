2bd3947879e2b19ec8520ca7f485fbcb9df5917b
om.bhallamudi <om.bhallamudi@gmail.com>
2015-03-20 21:40:50 +0530

    use value_of_css_property

--- firefox_ui_tests/remote/security/test_mixed_content.py
+++ firefox_ui_tests/remote/security/test_mixed_content.py
@@ -3,6 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from firefox_ui_harness.testcase import FirefoxTestCase
+from marionette_driver import Wait
 
 
 class TestMixedContent(FirefoxTestCase):
@@ -11,15 +12,29 @@ class TestMixedContent(FirefoxTestCase):
 
         self.url = 'https://mozqa.com/data/firefox/security/mixedcontent.html'
 
+    def tearDown(self):
+        try:
+            if self.browser.navbar.locationbar.identity_popup.is_open:
+                self.browser.navbar.locationbar.identity_popup.close(force=True)
+        finally:
+            FirefoxTestCase.tearDown(self)
+
     def test_mixed_content(self):
         with self.marionette.using_context('content'):
             self.marionette.navigate(self.url)
 
-        self.assertTrue('identity-icons-https-mixed-display' in
-                        self.navbar.locationbar.favicon.value_of_css_property('list-style-image'))
-        identity_popup = self.navbar.locationbar.identity_popup
+        favicon = self.browser.navbar.locationbar.favicon
+        self.wait_for_condition(lambda _: favicon.get_attribute('hidden') != 'none')
+        self.assertTrue("identity-icons-https-mixed-display" in
+                        favicon.value_of_css_property('list-style-image'))
+
+        identity_popup = self.browser.navbar.locationbar.identity_popup
         identity_popup.box.click()
 
-        self.wait_for_condition(lambda _: identity_popup.popup.is_displayed())
+        Wait(self.marionette).until(lambda _: identity_popup.is_open)
+
         self.assertEqual(identity_popup.encryption_label.text,
                          self.browser.get_property('identity.broken_loaded'))
+
+        identity_popup.close(force=True)
+        self.assertFalse(identity_popup.is_open)
